717d12252a4dd826f5986d0b8b16c2dac3d18791
Taras-Novitskyi <novitskyitaras@gmail.com>
2022-09-24 15:10:41 +0300

    add 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
 
+// const player = new Player('handstick', {
+//   id: 19231868,
+// 	width: 640,
+// });
+
+// const options = {
+//   id: 19231868,
+//   	width: 640,
+// };
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.addEventListener('timeupdate', onPlay);
+
+const onPlay = function (data) {
+	console.log(data);
+};
+
+player.on('play', onPlay);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('form');
+const email = input.querySelector('[name="email"]');
+const textarea = input.querySelector('[name="message"]');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+console.log(formData);
+
+populateForm();
+
+input.addEventListener('submit', onFormSubmit);
+input.addEventListener('input', throttle(onInput, 1000));
+
+function onInput(e) {
+	formData[e.target.name] = e.target.value;
+
+	localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+	e.preventDefault();
+	e.currentTarget.reset();
+	
+	localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function populateForm() {
+	const savedInfo = localStorage.getItem(LOCALSTORAGE_KEY)
+
+	if (savedInfo) {
+		const parsedInfo = JSON.parse(savedInfo);
+
+		if (parsedInfo.email) {
+			email.value = parsedInfo.email;
+			formData.email = parsedInfo.email;
+		} 
+		if (parsedInfo.message) {
+			textarea.value = parsedInfo.message;
+			formData.message = parsedInfo.message;
+		} 
+	}
+}
